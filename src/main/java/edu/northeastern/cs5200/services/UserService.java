d1c818f348451e65d809be56b6e8ed16c756646f
sinha.t <sinha.t@husky.neu.edu>
2018-08-17 15:57:07 -0400

    update service

--- src/main/java/edu/northeastern/cs5200/services/UserService.java
+++ src/main/java/edu/northeastern/cs5200/services/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +38,12 @@ public class UserService {
 	@GetMapping("/api/user/key/{key}")
 	public Iterable<User> findUsersByKey(@PathVariable("key") String key){
 		return userDao.findByKey(key);
+	}
+	
+	@PutMapping("/api/user/{id}")
+	public User updateUser(@PathVariable("id") int id,
+			@RequestBody User u) {
+		return userDao.updateUser(id, u);
 	}
 
 	@PostMapping("/api/user/authenticate")
